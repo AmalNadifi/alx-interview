393e6b32597eb5d422326df9e637f2460b7f7b83
AmalNadifi <amal.nadifi9@gmail.com>
2024-03-05 17:53:36 +0000

    Changed the approach to solve the task UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,40 +1,51 @@
 #!/usr/bin/python3
-""" The following script is for the UTF-8 Validation"""
+"""UTF-8 Validation"""
 
 
 def validUTF8(data):
-    """
-    This function determines if a given data set
-    represents a valid UTF-8 encoding.
+    """Determines if a given data set represents a valid UTF-8 encoding.
 
     Args:
         data (List[int]): The data set represented by a list of integers.
+
     Returns:
         bool: True if data is a valid UTF-8 encoding, else False.
     """
-    # Number of bytes in the current UTF-8 character
-    num_bytes = 0
+    # Initialize the number of bytes in the current UTF-8 character
+    remaining_bytes = 0
 
+    # Define bit masks for UTF-8 encoding
+    start_byte_mask = 1 << 7
+    cont_byte_mask = 1 << 6
+
+    # Iterate through each byte in the data set
     for byte in data:
-        # If this byte is part of a multibyte character
-        if num_bytes == 0:
-            if (byte >> 5) == 0b110:
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                num_bytes = 3
-            elif (byte >> 7) != 0:
-                # If most significant bit is set, it's not a valid start byte
+        # Initialize the mask byte to check the first bit
+        current_mask_byte = 1 << 7
+
+        # If this is the start byte of a UTF-8 character
+        if remaining_bytes == 0:
+            # Determine the number of bytes in the UTF-8 character
+            while current_mask_byte & byte:
+                remaining_bytes += 1
+                current_mask_byte = current_mask_byte >> 1
+
+            # If the number of bytes is invalid, return False
+            if remaining_bytes == 0:
+                continue
+            if remaining_bytes == 1 or remaining_bytes > 4:
                 return False
         else:
-            # If this byte is not a continuation byte
-            if (byte >> 6) != 0b10:
+            # If this is a continuation byte, check its format
+            if not (byte & start_byte_mask and not (byte & cont_byte_mask)):
                 return False
-            num_bytes -= 1
 
-    # If there are remaining bytes left
-    if num_bytes != 0:
-        return False
+        # Decrease the number of bytes remaining to check
+        remaining_bytes -= 1
 
-    return True
+    # If all bytes have been checked and no errors found, return True
+    if remaining_bytes == 0:
+        return True
+
+    # If there are remaining bytes, return False
+    return False
