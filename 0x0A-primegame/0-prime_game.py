c4ca1b3e73f23a002e9cc4f5b7105ac09b57846e
AmalNadifi <amal.nadifi9@gmail.com>
2024-04-14 23:27:19 -0700

    Change dthe approach

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,14 +3,19 @@
 which determines the winner in the Prime Game problem
 """
 
-def is_prime(num):
-    """This function checks if a number is prime"""
-    if num <= 1:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+def is_prime(n):
+    """This function checks if a number is prime
+    Args:
+        n (int): upper boundary of range. lower boundary is always 1
+    """
+    prime_numbers = []
+    is_prime = [True] * (n + 1)
+    for numb in range(2, n + 1):
+        if is_prime[numb]:
+            prime_numbers.append(numb)
+            for multiple in range(numb, n + 1, numb):
+                is_prime[multiple] = False
+    return prime_numbers
 
 def isWinner(x, nums):
     """
@@ -25,24 +30,22 @@ def isWinner(x, nums):
     - str or None: Name of the player with the most wins
     or None if the winner cannot be determined.
     """
+    if x is None or nums is None or x == 0 or nums == []:
+        return None
     maria_wins = 0
     ben_wins = 0
 
     # Iterate through each round
     for n in nums:
-        current_player = 0  # 0 for Maria, 1 for Ben
-
-        # Check if the starting number is prime
-        if is_prime(n):
-            if current_player == 0:
-                maria_wins += 1
-            else:
-                ben_wins += 1
+        prime = is_prime(n)
+        if len(prime) % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
 
     # Determine the overall winner
     if maria_wins > ben_wins:
-        return "Maria"
+        return 'Maria'
     elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+        return 'Ben'
+    return None
